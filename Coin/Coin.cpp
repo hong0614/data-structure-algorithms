59c9994d31ffe3adf6f7345c469a348de2ed4126
Elegant_gentleman <113571103+hong0614@users.noreply.github.com>
2023-12-25 03:17:36 +0800

    Add files via upload

--- Coin/Coin.cpp
+++ Coin/Coin.cpp
@@ -4,15 +4,54 @@
 #include <fstream>
 #include <chrono>
 #include <sstream>
-#include"coin.h"
 using namespace std;
 
+int minCoins(const vector<int>& availableCoins, int targetAmount)
+{
+    // Fixed denominations
+    vector<int> denominations = { 1, 5, 10, 25 };
+    int numCoinTypes = denominations.size();
+    vector<vector<int>> dp(numCoinTypes + 1, vector<int>(targetAmount + 1, INT_MAX));
 
-int main() {
-    ifstream inputFile("input.txt");
+    // Initializing the first row and column of the DP table
+    for (int i = 0; i <= targetAmount; i++)
+    {
+        dp[0][i] = INT_MAX;
+    }
+    for (int i = 0; i <= numCoinTypes; i++)
+    {
+        dp[i][0] = 0;
+    }
+
+    // Building up the DP table
+    for (int i = 1; i <= numCoinTypes; i++)
+    {
+        int coinValue = denominations[i - 1];
+        int coinLimit = availableCoins[i - 1];
+
+        for (int j = 0; j <= targetAmount; j++)
+        {
+            dp[i][j] = dp[i - 1][j];
+
+            for (int k = 1; k <= coinLimit && k * coinValue <= j; k++)
+            {
+                if (dp[i - 1][j - k * coinValue] != INT_MAX)
+                {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - k * coinValue] + k);
+                }
+            }
+        }
+    }
+
+    return dp[numCoinTypes][targetAmount] == INT_MAX ? -1 : dp[numCoinTypes][targetAmount];
+}
+
+int main()
+{
+    ifstream inputFile("input1.txt");
     ofstream outputFile("output.txt");
 
-    if (!inputFile) 
+    if (!inputFile)
     {
         cerr << "Error: file could not be opened" << endl;
         return -1;
@@ -21,7 +60,7 @@ int main() {
     vector<int> coinCounts(4);
     int target;
 
-    while (inputFile >> coinCounts[0] >> coinCounts[1] >> coinCounts[2] >> coinCounts[3]) 
+    while (inputFile >> coinCounts[0] >> coinCounts[1] >> coinCounts[2] >> coinCounts[3])
     {
         inputFile >> target;
 
@@ -30,11 +69,11 @@ int main() {
         auto end = chrono::high_resolution_clock::now();
         chrono::duration<double> elapsed = end - start;
 
-        if (result == -1) 
+        if (result == -1)
         {
             outputFile << "Solution does not exist." << endl;
         }
-        else 
+        else
         {
             outputFile << "Minimum number of coins required: " << result << endl;
         }
